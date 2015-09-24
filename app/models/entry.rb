b6e94bdf21a4feb7fe5d1f6e0d5fa3c867627419
David Tu <email": null>
2015-09-24 15:17:41 -0400

    add validations to entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ActiveRecord::Base
   has_many :entry_tags
   has_many :tags, through: :entry_tags
+
+  validates :name, :presence => true
+  validates :content, :presence => true
 end

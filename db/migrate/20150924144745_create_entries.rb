c240deb31719c1f0049adaafedef5e1e3a489e2e
David Tu <email": null>
2015-09-24 10:50:22 -0400

    add timestamps to migrations + null: false

--- db/migrate/20150924144745_create_entries.rb
+++ db/migrate/20150924144745_create_entries.rb
@@ -1,8 +1,10 @@
 class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
-      t.string :name
-      t.string :content
+      t.string :name, null: false
+      t.string :content, null: false
+
+      t.timestamps null: false
     end
   end
 end

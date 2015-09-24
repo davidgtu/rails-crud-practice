c268397baa6731ac9d0f72f6c2ebc8093a3d40ff
David Tu <email": null>
2015-09-24 11:32:59 -0400

    add views, models, associations

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,4 +2,27 @@ class EntriesController < ApplicationController
   def index
     @entries = Entry.all
   end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to entry_path(@entry.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @entry = Entry.find_by(id: params[:id])
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:name, :content)
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,4 @@
 class Entry < ActiveRecord::Base
+  has_many :entry_tags
+  has_many :tags, through: :entry_tags
 end
